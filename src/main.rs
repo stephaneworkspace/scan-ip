7a1fc08515bc0a6554e4debc3d2101554728adb6
stephaneworkspace <s.bressani@bluewin.ch>
2020-07-30 02:01:20 +0200

    good pratice rust with this small project (publish on master, not big project)

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,40 @@
  * Cli for scan with multi-thread ip range
  * By Stéphane Bressani
  * www.stephane-bressani.ch
+ *
+ * To do: Bot telegram notification or mail
  */
-use std::net::IpAddr;
+extern crate futures;
+extern crate tokio;
 
-struct Arguments {
+extern crate tokio_ping;
+
+use futures::{Future, Stream};
+// use std::net::IpAddr;
+
+// const MAX: u16 = 65535;
+
+/*struct Arguments {
     flag: String,
     ipaddr_begin: IpAddr,
     ipaddr_end: IpAddr,
     thread: u16,
-}
+}*/
 
 fn main() {
-    println!("Hello, world!");
+    let addr = std::env::args().nth(1).unwrap().parse().unwrap();
+
+    let pinger = tokio_ping::Pinger::new();
+    let stream = pinger.and_then(move |pinger| Ok(pinger.chain(addr).stream()));
+    let future = stream.and_then(|stream| {
+        stream.take(3).for_each(|mb_time| {
+            match mb_time {
+                Some(time) => println!("time={:?}", time),
+                None => println!("timeout"),
+            }
+            Ok(())
+        })
+    });
+
+    tokio::run(future.map_err(|err| eprintln!("Error: {}", err)))
 }
