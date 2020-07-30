b398dd86da1c5b2e7713b73774262bbc85269027
stephaneworkspace <s.bressani@bluewin.ch>
2020-07-30 02:16:13 +0200

    good pratice rust with this small project (publish on master, not big project)

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,12 @@
  *
  * To do: Bot telegram notification or mail
  */
-extern crate futures;
-extern crate tokio;
+// extern crate futures;
+// extern crate tokio;
 
-extern crate tokio_ping;
+// extern crate tokio_ping;
 
-use futures::{Future, Stream};
+// use futures::{Future, Stream};
 // use std::net::IpAddr;
 
 // const MAX: u16 = 65535;
@@ -21,8 +21,41 @@ use futures::{Future, Stream};
     ipaddr_end: IpAddr,
     thread: u16,
 }*/
+/*
+extern crate hyper;
+use hyper::Client;
+use hyper::body::HttpBody as _;
+use tokio::io::{stdout, AsyncWriteExt as _};
+use hyper_tls::HttpsConnector;
 
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    // Check health of https
+
+    // http only
+    // let client = Client::new();
+
+    // http or https connections
+    let client = Client::builder().build::<_, hyper::Body>(HttpsConnector::new());
+
+    let mut resp = client.get("https://www.stephane-bressani.ch".parse()?).await?;
+
+    println!("Response: {}", resp.status());
+
+    while let Some(chunk) = resp.body_mut().data().await {
+        stdout().write_all(&chunk?).await?;
+    }
+
+    Ok(())
+}
+*/
 fn main() {
+    // Ip range with arg
+    // -> Todo
+
+    // Ping fn
+    // 29.07 OK work but only with sudo
+    /*
     let addr = std::env::args().nth(1).unwrap().parse().unwrap();
 
     let pinger = tokio_ping::Pinger::new();
@@ -38,4 +71,5 @@ fn main() {
     });
 
     tokio::run(future.map_err(|err| eprintln!("Error: {}", err)))
+    */
 }
